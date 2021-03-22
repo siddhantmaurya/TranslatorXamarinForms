80e693fb9f74372b648922841a75e5b05b300af9
siddhant maurya <maurya2010boby@outlook.com>
2021-03-22 18:25:52 +0530

    Add project files.

---
+++ TranslatorXamarinForms/TranslatorXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TranslatorXamarinForms
+{
+    public partial class MainPage : ContentPage
+    {
+        private static readonly string subscriptionKey = "<YOUR SERVICE KEY>";
+        private static readonly string endpoint = "<YOUR ENDPOINT>";
+
+        // Add your location, also known as region. The default is global.
+        // This is required if using a Cognitive Services resource.
+        private static readonly string location = "<YOUR LOCATION>";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Translate_Tapped(object sender, EventArgs e)
+        {
+            TargetText.Text = await TranslateText(SourceText.Text);
+        }
+        
+        static async Task<string> TranslateText(string textToTranslate)
+        {
+            // Input and output languages are defined as parameters.
+            string route = "/translate?api-version=3.0&from=en&to=es";
+            object[] body = new object[] { new { Text = textToTranslate } };
+            var requestBody = JsonConvert.SerializeObject(body);
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                // Build the request.
+                request.Method = HttpMethod.Post;
+                request.RequestUri = new Uri(endpoint + route);
+                request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+                // Send the request and get response.
+                HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                // Read response as a string.
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<Dictionary<string, List<Dictionary<string, string>>>>>(responseBody);
+                var translation = result[0]["translations"][0]["text"];
+                return translation;
+            }
+        }
+
+    }
+}
